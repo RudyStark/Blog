b6e08ab024508872969269c25782e8168fe1ddb7
RudyStark <89338357+RudyStark@users.noreply.github.com>
2022-09-28 11:49:43 +0200

    installation du projet

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo "Hello World!";
+use App\Controller\TestController;
+
+require(dirname(__DIR__) . '/vendor/autoload.php');
+
+$controller = new TestController;
+
+$controller->hello();
